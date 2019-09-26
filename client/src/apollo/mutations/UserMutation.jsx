7a746857d47b888dc71e8369f5d4dae2bc746ce4
Hayze0 <zohair.davud@epitech.eu>
2019-09-26 15:46:27 +0200

    Form inscription

--- client/src/apollo/mutations/UserMutation.jsx
+++ client/src/apollo/mutations/UserMutation.jsx
@@ -1,16 +1,4 @@
 import React, { useState } from 'react';
 import gql from 'graphql-tag';
-import { useMutation } from '@apollo/react-hooks';
-export const SignUp = (email, username, password) => {
-    const [userData, setUserData] = useState([])
-    const [newUser] = useMutation(NEW_USER);
-    setUserData([
-        email,
-        username,
-        password
-    ]);
-    const NEW_USER = gql`
-    newUser(email: String, username: String, password: String) {
-         token
-    }`;
-}
+ 
+
--- client/src/components/Forms/FormInscription/FormInscription.jsx
+++ client/src/components/Forms/FormInscription/FormInscription.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import style from './style.module.scss';
-const FormInscription = (props) => {
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
 
+const FormInscription = (props) => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const Mutation = () =>{
+    const  NEW_USER = gql`
+    newUser(email: String, username: String, password: String) {
+        email
+        username     
+    }`
+    const [addData, data] = useMutation(NEW_USER);  
+    addData({email: email,username: username,password: password});
+    console.log(data)
+      return data;
+  
+  }
+  
+
+    const userSignup =  (e) => {
+      e.preventDefault();
+      Mutation();
+    }
+     
+    
 return (
     <div>
         <h1 className={style.titleInscription}>Ghost-Composer</h1>
    <div className={style.formInscritpion}>
        <h3>Inscrivez-vous gratuitement</h3>
-<Form>
+<Form onSubmit={userSignup}>
     <FormGroup row>
     <Col sm={6}>
     <Button color="secondary" size="lg" block>Connect  <i class='fab fa-facebook'></i></Button>
@@ -22,26 +48,29 @@ return (
           <Col sm={6}>
           <Label for="exampleEmail" sm={2}>Email</Label>
 
-            <Input type="email" name="email" id="exampleEmail" placeholder="Enter your Email.." />
+            <Input onChange={e => {
+               setEmail(e.target.value)}
+               }    type="email" name="email" id="exampleUsername" value={email} placeholder="Enter your Email.." />
           </Col>
           <Col sm={6}>
           <Label for="exampleEmail" sm={2}>Username</Label>
 
-            <Input type="email" name="email" id="exampleEmail" placeholder="Enter your Username..." />
+            <Input   onChange={e => { setUsername(e.target.value)}}  value={username} type="name" name="username" id="exampleEmail" placeholder="Enter your Username..." />
           </Col>  
     </FormGroup>
     <FormGroup row>
     <Col sm={12}>
           <Label for="exampleEmail" sm={2}>Password</Label>
 
-          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+          <Input value={password} onChange={e => { setPassword(e.target.value)}}  type="password" name="password"  id="examplePassword" placeholder="password placeholder" />
           </Col>
         
     </FormGroup>
     <div className={style.validationButton}>
    <FormGroup row>
         <Col sm={6}>
-        <Button color="secondary" size="lg" block>Inscription <i class='fas fa-chevron-circle-right'></i></Button>
+        <Button  color="secondary" size="lg" type="submit" block>Inscription <i class='fas fa-chevron-circle-right'></i>
+        </Button>
         </Col>
     </FormGroup>
     </div>
@@ -51,4 +80,5 @@ return (
   </div>
 );
 }
+
 export default FormInscription;
